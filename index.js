32db82f924f11eba73ec4726c0181003a4965962
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-31 11:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your solution in this file!
+const driver={};
+function updateDriverWithKeyAndValue(){}
+function destructivelyUpdateDriverWithKeyAndValue(){}
+function deleteFromDriverByKey(){}
+function destructivelyDeleteFromDriverByKey(){}
